d2a3bc1b5660568b2418757a83f7c53350987982
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-03-15 21:10:49 -0300

    feat: sistema de login para o sistema

---
+++ corp-back-end/src/main/java/br/com/emanoelCompany/corp/controllers/LoginController.java
@@ -0,0 +1,41 @@
+package br.com.emanoelCompany.corp.controllers;
+
+
+import br.com.emanoelCompany.corp.services.AdministratorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @Autowired
+    private AdministratorService administratorService;
+
+    @GetMapping()
+    public String showLoginPage() {
+        return "login";
+    }
+
+    @PostMapping()
+    public String login(String usuario, String senha, Model model) {
+        if (usuario != null && !usuario.isEmpty() && senha != null && !senha.isEmpty()) {
+            if (administratorService.administradorExiste(usuario, senha)) {
+                    return "redirect:/home";
+            } else {
+                model.addAttribute("error", "Usuário ou senha inválidos");
+                return "home";
+            }
+        } else {
+            model.addAttribute("error", "Por favor, preencha todos os campos");
+            return "login";
+        }
+
+    }
+}
+
