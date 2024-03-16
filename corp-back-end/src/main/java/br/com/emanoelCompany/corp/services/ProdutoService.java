d75dd472c57961d0c308549f9c8f2e1f34bb949c
PierreOF <marcos.pierre@upe.br>
2024-03-16 20:50:26 -0300

    <feat> backend vender um produto

--- corp-back-end/src/main/java/br/com/emanoelCompany/corp/services/ProdutoService.java
+++ corp-back-end/src/main/java/br/com/emanoelCompany/corp/services/ProdutoService.java
@@ -4,6 +4,7 @@ import br.com.emanoelCompany.corp.DTO.ProdutoDTO;
 import br.com.emanoelCompany.corp.exceptions.ConversaoDataProdutoDTOExeption;
 import br.com.emanoelCompany.corp.exceptions.ProdutoDTOValidationException;
 import br.com.emanoelCompany.corp.exceptions.ProdutoNaoEncontradoException;
+import br.com.emanoelCompany.corp.exceptions.QuantidadeInsuficienteException;
 import br.com.emanoelCompany.corp.model.Produto;
 import br.com.emanoelCompany.corp.repository.ProdutoRepository;
 import org.hibernate.StaleObjectStateException;
@@ -12,7 +13,9 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -101,7 +104,7 @@ public class ProdutoService{
         }
     }
 
-    public boolean deletar(Long id){
+    public void deletar(Long id){
         Optional<Produto> produtoOptional = produtoRepository.findById(id);
         if(produtoOptional.isPresent()){
             try {
@@ -109,7 +112,6 @@ public class ProdutoService{
             } catch (StaleObjectStateException e) {
                 throw new ProdutoNaoEncontradoException("O produto foi atualizado ou excluído por outra transação.");
             }
-            return true;
         }else {
             throw new ProdutoNaoEncontradoException();
         }
@@ -118,7 +120,6 @@ public class ProdutoService{
         if (produtoDTO.id() == null) {
             throw new ProdutoDTOValidationException("Informe o ID do produto para atualizar!");
         }
-
         Produto produto = produtoRepository.findById(produtoDTO.id())
                 .orElseThrow(() -> new ProdutoNaoEncontradoException("O ID fornecido não condiz com nenhum produto em nosso estoque!"));
 
@@ -128,8 +129,25 @@ public class ProdutoService{
         produto.setPrecoTotal(produto.valorTotal(produtoDTO.quantidade(), produtoDTO.preco()));
         Produto produtoAtualizado = produtoRepository.save(produto);
 
-
         return  convertToDTO(produtoAtualizado);
     }
+    public void vender(Map<Long,Integer> vendaMap){
+
+        for (Long id : vendaMap.keySet()){
+            int quantidade = vendaMap.get(id);
+            Produto produto = produtoRepository.findById(id)
+                    .orElseThrow(() -> new ProdutoNaoEncontradoException("O ID fornecido não condiz com nenhum produto em estoque "));
+
+            int quantidadeAtualizada =  (produto.getQuantidade()-quantidade);
+            if(quantidadeAtualizada < 0){
+                throw new QuantidadeInsuficienteException();
+            }
+
+            produto.setQuantidade(quantidadeAtualizada);
+            produtoRepository.save(produto);
+        }
+    }
+
+
 
 }
